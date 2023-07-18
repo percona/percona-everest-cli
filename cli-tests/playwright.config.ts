67e57c104e05140df3da3d50d3aa470ac458a2e2
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2023-07-18 16:37:50 +0300

    EVEREST-112 CLI tests base (#48)
    * EVEREST-112 initial setup for cli tests
    
    * EVEREST-112 use robot token
    
    * EVEREST-112 fix build
    
    * EVEREST-112 add report, fix tests

---
+++ cli-tests/playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['github'],
+    ['list'],
+    ['html', { open: 'never', outputFolder: './test-report' }]
+  ],
+  use: {
+    trace: 'off',
+  },
+});
