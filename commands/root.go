32f2693060b9deefeafdeffab5414c9993af99bc
Michal Kralik <michal.kralik@percona.com>
2023-07-20 14:29:23 +0200

    Add global verbose flag
    - Verbose flag enables debug logger

--- commands/root.go
+++ commands/root.go
@@ -1,13 +1,28 @@
 // Package commands implements main logic for cli commands.
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
 
 // NewRootCmd creates a new root command for the cli.
 func NewRootCmd() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use: "everest",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			verbose, err := cmd.Flags().GetBool("verbose")
+			if err != nil {
+				logrus.Warn(`Could not parse "verbose" flag`)
+			}
+
+			if verbose {
+				logrus.SetLevel(logrus.DebugLevel)
+			}
+		},
 	}
+
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "Enable verbose mode")
 
 	rootCmd.AddCommand(newInstallCmd())
 
