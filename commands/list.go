2c8c74c028944cd9e31522a9121fd4341c781931
Michal Kralik <michal.kralik@percona.com>
2023-07-20 15:28:32 +0200

    EVEREST-141: list database engines (#49)
    - Adds support for --json flag

---
+++ commands/list.go
@@ -0,0 +1,18 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/percona/percona-everest-cli/commands/list"
+)
+
+func newListCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "list",
+	}
+
+	cmd.AddCommand(list.NewDatabaseEnginesCmd())
+	cmd.AddCommand(list.NewVersionsCmd())
+
+	return cmd
+}
---
+++ commands/list/database_engines.go
@@ -0,0 +1,68 @@
+// Package list holds logic for list commands.
+package list
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/percona/percona-everest-backend/client"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	everestClient "github.com/percona/percona-everest-cli/pkg/everest/client"
+	"github.com/percona/percona-everest-cli/pkg/list"
+	"github.com/percona/percona-everest-cli/pkg/output"
+)
+
+// NewDatabaseEnginesCmd returns a new database engines command.
+func NewDatabaseEnginesCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "databaseengines",
+		Aliases: []string{"databaseengine", "dbengines", "dbengine"},
+		Run: func(cmd *cobra.Command, args []string) {
+			initDatabaseEnginesViperFlags(cmd)
+
+			c, err := parseDatabaseEnginesConfig()
+			if err != nil {
+				os.Exit(1)
+			}
+
+			everestCl, err := client.NewClient(fmt.Sprintf("%s/v1", c.Everest.Endpoint))
+			if err != nil {
+				logrus.Error(err)
+				os.Exit(1)
+			}
+
+			everestClConnector := everestClient.NewEverest(everestCl)
+			command := list.NewDatabaseEngines(c, everestClConnector)
+			dbEngines, err := command.Run(cmd.Context())
+			if err != nil {
+				logrus.Error(err)
+				os.Exit(1)
+			}
+
+			output.PrintOutput(cmd, dbEngines)
+		},
+	}
+
+	initDatabaseEnginesFlags(cmd)
+
+	return cmd
+}
+
+func initDatabaseEnginesFlags(cmd *cobra.Command) {
+	cmd.Flags().String("everest.endpoint", "http://127.0.0.1:8081", "Everest endpoint URL")
+	cmd.Flags().String("kubernetes-id", "", "Kubernetes cluster ID")
+}
+
+func initDatabaseEnginesViperFlags(cmd *cobra.Command) {
+	viper.BindPFlag("everest.endpoint", cmd.Flags().Lookup("everest.endpoint")) //nolint:errcheck,gosec
+	viper.BindPFlag("kubernetes-id", cmd.Flags().Lookup("kubernetes-id"))       //nolint:errcheck,gosec
+}
+
+func parseDatabaseEnginesConfig() (*list.DBEnginesConfig, error) {
+	c := &list.DBEnginesConfig{}
+	err := viper.Unmarshal(c)
+	return c, err
+}
