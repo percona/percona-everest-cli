120a49bd2c2ed591954c072b77197524f6aea5b2
Michal Kralik <michal.kralik@percona.com>
2023-08-02 08:04:06 +0200

    Linters

--- pkg/delete/cluster.go
+++ pkg/delete/cluster.go
@@ -71,11 +71,9 @@ func (c *Cluster) Run(ctx context.Context) error {
 	}
 
 	if !c.config.Force {
-		//nolint:forbidigo,govet
-		fmt.Println(`
-You are about to delete a Kubernetes cluster from Everest.
-This will delete all monitoring resources deployed by Everest from the Kubernetes cluster.
-`)
+		msg := `You are about to delete a Kubernetes cluster from Everest.
+This will delete all monitoring resources deployed by Everest from the Kubernetes cluster.`
+		fmt.Printf("\n%s\n\n", msg) //nolint:forbidigo
 		confirm := &survey.Confirm{
 			Message: fmt.Sprintf("Are you sure you want to delete the %q Kubernetes cluster in Everest?", c.config.Name),
 		}
