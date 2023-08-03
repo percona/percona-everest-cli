8d4de1e0c71f1ef8071e171a740dbd18a1b51d84
Andrew Minkin <andrew.minkin@percona.com>
2023-08-03 15:16:16 +0600

    EVEREST-107 Fixed linter

--- pkg/install/operators.go
+++ pkg/install/operators.go
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // Package install holds the main logic for installation commands.
+
+// Package install ...
 package install
 
 import (
