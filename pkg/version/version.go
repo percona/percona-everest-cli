e82442c76b293c04584c4a7e76cceef7861410b2
Andrew Minkin <andrew.minkin@percona.com>
2023-08-08 19:51:55 +0600

    EVEREST-230 Shut up you linter

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -13,8 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package version holds release version information
+// Package version holds release version information.
 package version
 
-// Version of the everest cli
+// Version of the everest cli.
 var Version string
