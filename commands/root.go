9b5a6b84ebf227206b1670035147f963da16edf1
Michal Kralik <michal.kralik@percona.com>
2023-08-22 11:57:12 +0200

    EVEREST-292: fix cli name (#105)

--- commands/root.go
+++ commands/root.go
@@ -26,7 +26,7 @@ import (
 // NewRootCmd creates a new root command for the cli.
 func NewRootCmd(l *zap.SugaredLogger) *cobra.Command {
 	rootCmd := &cobra.Command{
-		Use: "everest",
+		Use: "everestctl",
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			logger.InitLoggerInRootCmd(cmd, l)
 			l.Debug("Debug logging enabled")
