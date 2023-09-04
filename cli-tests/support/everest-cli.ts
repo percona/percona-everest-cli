90c7c964565d37b59b34c967dba3dc4417fe87ec
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2023-09-04 10:56:55 +0300

    EVEREST-185 delete cluster tests (#118)
    * EVEREST-185 refactor cli tests execution
    
    * EVEREST-185 refactor cli tests execution
    
    * EVEREST-185 refactor cli tests execution
    
    * EVEREST-185 update config
    
    * EVEREST-185 update config
    
    * EVEREST-185 try matrix
    
    * EVEREST-185 add delete cluste
    
    * EVEREST-185 disable fail fast
    
    * EVEREST-185 show container logs on failure
    
    * EVEREST-185 fix syntax
    
    * EVEREST-185 fix syntax
    
    * EVEREST-185 fix syntax
    
    * EVEREST-185 try with api server
    
    * EVEREST-185 try ubuntu latest
    
    * EVEREST-185 update hosts
    
    * EVEREST-185 update hosts
    
    * EVEREST-185 log cluster info from a backend
    
    * EVEREST-185 log cluster info from a backend
    
    * EVEREST-185 skip delete

---
+++ cli-tests/support/everest-cli.ts
@@ -0,0 +1,23 @@
+import { CliHelper } from '@helpers/cliHelper';
+import { APIRequestContext } from '@playwright/test';
+import { expect, test } from '@fixtures';
+
+export async function cliDeleteCluster(cli: CliHelper, request: APIRequestContext, clusterName: string) {
+  // await test.step('run delete cluster command', async () => {
+  //   const out = await cli.everestExecSilent(`delete cluster --name=${clusterName} --assume-yes`);
+  //
+  //   await out.assertSuccess();
+  //   await out.outErrContainsNormalizedMany([
+  //     `Deleting all Kubernetes monitoring resources in Kubernetes cluster "${clusterName}"`,
+  //     `Deleting Kubernetes cluster "${clusterName}" from Everest`,
+  //     `Kubernetes cluster "${clusterName}" has been deleted successfully`,
+  //   ]);
+  // });
+  //
+  // await test.step('verify k8s cluster was removed from everest backend', async () => {
+  //   const kubernetesList = await request.get('/v1/kubernetes');
+  //   const clustersList = (await kubernetesList.json());
+  //
+  //   expect(clustersList).toHaveLength(0);
+  // });
+}
