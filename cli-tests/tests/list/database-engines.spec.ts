dbb32e24761794bd7fc9e5d9ca63c21d6c740f20
Andrew Minkin <andrew.minkin@percona.com>
2023-10-09 19:41:10 +0600

    EVEREST-358 Disable list commands (#165)

--- cli-tests/tests/list/database-engines.spec.ts
+++ cli-tests/tests/list/database-engines.spec.ts
@@ -12,45 +12,45 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { test, expect } from '@fixtures';
-import { waitForDBEngines } from '@tests/support/kubernetes';
-
-let kubernetesId = '';
-
-test.beforeAll(async ({ cli, request }) => {
-  const kubernetesList = await request.get('/v1/kubernetes');
-
-  kubernetesId = (await kubernetesList.json())[0].id;
-  expect(kubernetesId).toBeTruthy();
-
-  // Wait until all dbengines are ready
-  await expect.poll(() => waitForDBEngines(cli), {
-    message: 'dbengine not yet installed',
-    intervals: [1000],
-    timeout: 240 * 1000,
-  }).toBe(true);
-});
-
-test.describe('Database engines', async () => {
-  test('list', async ({ cli }) => {
-    const out = await cli.everestExecSilent(`list databaseengines --kubernetes-id ${kubernetesId}`);
-
-    await out.assertSuccess();
-    await out.outContainsNormalizedMany([
-      'postgresql',
-      'psmdb',
-      'pxc',
-    ]);
-  });
-
-  test('list json', async ({ cli }) => {
-    const out = await cli.everestExecSilent(`--json list databaseengines --kubernetes-id ${kubernetesId}`);
-
-    await out.assertSuccess();
-    const res = JSON.parse(out.stdout);
-
-    expect(res?.postgresql?.version).toBeTruthy();
-    expect(res?.psmdb?.version).toBeTruthy();
-    expect(res?.pxc?.version).toBeTruthy();
-  });
-});
+//import { test, expect } from '@fixtures';
+//import { waitForDBEngines } from '@tests/support/kubernetes';
+//
+//let kubernetesId = '';
+//
+//test.beforeAll(async ({ cli, request }) => {
+//  const kubernetesList = await request.get('/v1/kubernetes');
+//
+//  kubernetesId = (await kubernetesList.json())[0].id;
+//  expect(kubernetesId).toBeTruthy();
+//
+//  // Wait until all dbengines are ready
+//  await expect.poll(() => waitForDBEngines(cli), {
+//    message: 'dbengine not yet installed',
+//    intervals: [1000],
+//    timeout: 240 * 1000,
+//  }).toBe(true);
+//});
+//
+//test.describe('Database engines', async () => {
+//  test('list', async ({ cli }) => {
+//    const out = await cli.everestExecSilent(`list databaseengines --kubernetes-id ${kubernetesId}`);
+//
+//    await out.assertSuccess();
+//    await out.outContainsNormalizedMany([
+//      'postgresql',
+//      'psmdb',
+//      'pxc',
+//    ]);
+//  });
+//
+//  test('list json', async ({ cli }) => {
+//    const out = await cli.everestExecSilent(`--json list databaseengines --kubernetes-id ${kubernetesId}`);
+//
+//    await out.assertSuccess();
+//    const res = JSON.parse(out.stdout);
+//
+//    expect(res?.postgresql?.version).toBeTruthy();
+//    expect(res?.psmdb?.version).toBeTruthy();
+//    expect(res?.pxc?.version).toBeTruthy();
+//  });
+//});
