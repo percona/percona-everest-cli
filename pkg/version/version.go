cb70dbbf5333ddb7ba331818142f8a215936ee38
Andrew Minkin <andrew.minkin@percona.com>
2023-10-13 15:09:04 +0600

    EVEREST-488 Fixed linter

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,3 +1,20 @@
+// percona-everest-cli
+// Copyright (C) 2023 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// Package install holds the main logic for installation commands.
+
+// Package version implements version reporting command to the end user.
 package version
 
 import (
@@ -12,13 +29,13 @@ const (
 
 var (
 	// ProjectName is a component name, e.g. everestctl.
-	ProjectName string
+	ProjectName string //nolint:gochecknoglobals
 	// Version is a component version e.g. v0.3.0-1-a93bef.
-	Version string
+	Version string //nolint:gochecknoglobals
 	// FullCommit is a git commit hash.
-	FullCommit string
+	FullCommit string //nolint:gochecknoglobals
 	// CatalogImage is a image path for OLM catalog
-	catalogImage string
+	catalogImage string //nolint:gochecknoglobals
 )
 
 func CatalogImage() string {
