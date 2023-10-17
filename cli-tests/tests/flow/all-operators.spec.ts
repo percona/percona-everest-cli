a79128b96a9bd6b0f6c1c2a96e34a5aa12442e57
Oksana Grishchenko <oksana.grishchenko@percona.com>
2023-10-17 16:38:52 +0200

    fix test

--- cli-tests/tests/flow/all-operators.spec.ts
+++ cli-tests/tests/flow/all-operators.spec.ts
@@ -75,27 +75,23 @@ test.describe('Everest CLI install operators', async () => {
       // check that the telemetry IS NOT disabled by default
       let out = await cli.exec('kubectl get deployments/percona-xtradb-cluster-operator --namespace=percona-everest -o yaml');
 
-      await out.outContainsNormalizedMany([
-        `- name: DISABLE_TELEMETRY
-     value: \\"false\\"`,
-      ]);
+      await out.outContains(
+        'name: DISABLE_TELEMETRY\n          value: "true"',
+      );
 
-      await cli.exec('export DISABLE_TELEMETRY=true');
-      out = await cli.everestExecSkipWizard(`install operators --backup.enable=0 --monitoring.enable=0 --name=${clusterName}`);
+      out = await cli.everestExecSkipWizardWithEnv('install operators --backup.enable=0 --monitoring.enable=0 --name=aaa', 'DISABLE_TELEMETRY=true');
       await out.assertSuccess();
       await out.outErrContainsNormalizedMany([
         'percona-xtradb-cluster-operator operator has been installed',
         'percona-server-mongodb-operator operator has been installed',
         'percona-postgresql-operator operator has been installed',
         'everest-operator operator has been installed',
-        'Connected Kubernetes cluster to Everest',
       ]);
       // check that the telemetry IS disabled
-      out = await cli.exec('kubectl get deployments/percona-xtradb-cluster-operator --namespace=percona-everest');
-      await out.outContainsNormalizedMany([
-        `- name: DISABLE_TELEMETRY
-     value: \\"true\\"`,
-      ]);
+      out = await cli.exec('kubectl get deployments/percona-xtradb-cluster-operator --namespace=percona-everest -o yaml');
+      await out.outContains(
+        'name: DISABLE_TELEMETRY\n          value: "true"',
+      );
     });
   });
 });
