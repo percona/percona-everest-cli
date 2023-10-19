507413338642906abd3edf53e2d178b6482a7c72
Andrew Minkin <andrew.minkin@percona.com>
2023-10-19 19:16:07 +0600

    EVEREST-511 Fixed timeout

--- pkg/kubernetes/client/client.go
+++ pkg/kubernetes/client/client.go
@@ -1106,18 +1106,6 @@ func (c *Client) CreateSubscriptionForCatalog(ctx context.Context, namespace, na
 		Subscriptions(namespace).
 		Create(ctx, subscription, metav1.CreateOptions{})
 	if err != nil {
-		if apierrors.IsAlreadyExists(err) && name != "everest-operator" {
-			bytes, err := json.Marshal(subscription)
-			if err != nil {
-				return sub, err
-			}
-			sub, err = operatorClient.
-				OperatorsV1alpha1().
-				Subscriptions(namespace).
-				Patch(ctx, name, types.MergePatchType, bytes, metav1.PatchOptions{})
-
-			return sub, err
-		}
 		return sub, err
 	}
 	return sub, nil
