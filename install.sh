0f0d079f86dfc76508d3d33a6a772072079c3233
Andrew Minkin <andrew.minkin@percona.com>
2023-11-07 17:10:02 +0600

    Drop docker compose

--- install.sh
+++ install.sh
@@ -35,13 +35,6 @@ echo "Downloading the latest release of Percona Everest CLI"
 echo "https://github.com/percona/percona-everest-cli/releases/download/${latest_release}/everestctl-$os-$arch"
 curl -sL  https://github.com/percona/percona-everest-cli/releases/download/${latest_release}/everestctl-$os-$arch -o everestctl
 chmod +x everestctl
-echo "Deploying Backends using docker compose"
-curl -sL  https://raw.githubusercontent.com/percona/percona-everest-backend/main/deploy/quickstart-compose.yml -o quickstart.yml
-docker compose -f quickstart.yml up -d
-
-while ! curl -s http://127.0.0.1:8080 &> /dev/null ; do
-	sleep 0.2
-done
 
 # If KUBECONFIG is set let the user know we are using it
 if [[ -n "${KUBECONFIG}" ]]; then
