a7591381d39c6b7219327396864795c042afe77e
Andrew Minkin <andrew.minkin@percona.com>
2023-12-14 14:44:51 +0100

    EVEREST-702 cleanup

--- pkg/monitoring/enable.go
+++ pkg/monitoring/enable.go
@@ -12,9 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// Package install holds the main logic for installation commands.
 
-// Package install ...
+// Package monitoring holds the main logic for provision monitoring
 package monitoring
 
 import (
