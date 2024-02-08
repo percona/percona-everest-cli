93b56da2a42911d24593d2762b20989383aa2dcd
Diogo Recharte <diogo.recharte@percona.com>
2024-02-08 10:50:46 +0000

    EVEREST-633 remove deprecated code

--- commands/root.go
+++ commands/root.go
@@ -37,9 +37,6 @@ func NewRootCmd(l *zap.SugaredLogger) *cobra.Command {
 	rootCmd.PersistentFlags().Bool("json", false, "Set output type to JSON")
 
 	rootCmd.AddCommand(newInstallCmd(l))
-	// rootCmd.AddCommand(newProvisionCmd(l))
-	// rootCmd.AddCommand(newListCmd(l))
-	// rootCmd.AddCommand(newDeleteCmd(l))
 	rootCmd.AddCommand(newTokenCmd(l))
 	rootCmd.AddCommand(newVersionCmd(l))
 	rootCmd.AddCommand(newUpgradeCmd(l))
